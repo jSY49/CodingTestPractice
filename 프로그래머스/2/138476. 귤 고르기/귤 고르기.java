a37ffdf2795362520558438d81ef5fd46977fa13
Yeon <96411866+jSY49@users.noreply.github.com>
2026-04-29 21:21:34 +0900

    [level 2] Title: 귤 고르기, Time: 67.43 ms, Memory: 98.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        
+        // 크기 당 개수 저장 
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int t : tangerine) {
+            map.put(t, map.getOrDefault(t, 0) + 1);
+        }
+        
+        List<Integer> counts = new ArrayList<>(map.values());
+        counts.sort(Collections.reverseOrder());  
+        
+        for(int count : counts){
+             k -= count;
+            answer++;
+            
+            if(k <= 0 ){
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
