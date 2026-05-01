9f339ad051d5a3eeefe9012b1ef4d16c47f88986
Yeon <96411866+jSY49@users.noreply.github.com>
2026-05-01 22:02:59 +0900

    [level 2] Title: 할인 행사, Time: 185.59 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -8,16 +8,13 @@ class Solution {
             targetMap.put(want[i], number[i]);
         }
         
-        for(int i=0 ; i< discount.length;i++){
+        for(int i=0 ; i <= discount.length - 10;i++){
             
             Map<String, Integer> currentMap = new HashMap<>();
             for (int j = i; j < i + 10; j++) {
-                
-                if(j < discount.length){
-                    if (targetMap.containsKey(discount[j])) {
+                if (targetMap.containsKey(discount[j])) {
                         currentMap.put(discount[j], currentMap.getOrDefault(discount[j], 0) + 1);
-                    }    
-                }
+                    }  
             }
             
             if(targetMap.equals(currentMap)){
