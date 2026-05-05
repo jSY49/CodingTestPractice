91c750e9922550371a12edc1bd93dcbc93a69145
Yeon <96411866+jSY49@users.noreply.github.com>
2026-05-05 21:33:21 +0900

    [level 1] Title: 2016년, Time: 1.35 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,11 @@
+import java.time.*;
+
+class Solution {
+    public String solution(int a, int b) {
+        return LocalDate.of(2016,a,b)
+            .getDayOfWeek()
+            .name()
+            .substring(0,3)
+            .toUpperCase();
+    }
+}
